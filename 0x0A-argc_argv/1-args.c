c702bdbc9bd43811ad03b56ac23028eb194016fb
adebayoisreal <109547774+adebayoisreal@users.noreply.github.com>
2022-11-04 23:13:25 -0700

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - code
+ * @argc: code
+ * @argv: code
+ *
+ * Description: code
+ * Return: code
+ */
+int __attribute__((unused)) main(int argc, char *argv[])
+{
+	(void) *argv;
+	printf("%d\n", (argc - 1));
+
+	return (0);
+}
