c25a5542190351227115f4f1c0f838d2e6e27173
adebayoisreal <109547774+adebayoisreal@users.noreply.github.com>
2022-11-04 23:12:50 -0700

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - code
+ * @argc: code
+ * @argv: code
+ *
+ * Description: code
+ * Return: code
+ */
+int __attribute__((unused)) main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
