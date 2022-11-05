3920a618e3ea5823dbeb99537094ac492f66503f
adebayoisreal <109547774+adebayoisreal@users.noreply.github.com>
2022-11-04 23:49:57 -0700

    Create 3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _strcmp - code
+ * @s1: code
+ * @s2: code
+ * Return: code
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int s1len = 0, s2len = 0;
+	char *s1start = s1, *s2start = s2;
+
+	while (*s1)
+	{
+		s1len++;
+		s1++;
+	}
+
+	s1 = s1start;
+
+	while (*s2)
+	{
+		s2len++;
+		s2++;
+	}
+
+	s2 = s2start;
+
+	if (s1 > s2)
+		return (15);
+
+	else if (s1 < s2)
+		return (-15);
+
+	while (*s1)
+	{
+		if (*s1 < *s2)
+			return (-15);
+		if (*s1 > *s2)
+			return (15);
+		s1++;
+		s2++;
+	}
+	return (0);
+}
