bed35e99bd2866096e7239c470b495e0f37d7881
adebayoisreal <109547774+adebayoisreal@users.noreply.github.com>
2022-11-04 23:14:07 -0700

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - code
+ * @argc: code
+ * @argv: code
+ *
+ * Description: code
+ * Return: code
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
